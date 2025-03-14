09c4264cf04d7c8c1dc9d7f5fa64329aca89122b
Takshil Kunadia <takshilkunadia@gmail.com>
2025-03-14 17:15:58 +0530

    feat(print-circ): Bootstrap plugin

---
+++ newspack-print-circ-integration.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: Newspack Print Circulation Integration
+ * Description: Plugin to integrate Newspack with Print Circulation Systems.
+ * Version: 1.0.0
+ * Author: Automattic
+ * Author URI: https://newspack.com/
+ * License: GPL2
+ * Text Domain: newspack-print-circulation
+ *
+ * @package Newspack\PrintCirculation
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'NEWSPACK_PRINT_CIRC_INTEGRATION_VERSION', '1.0.0' );
+
+// Define NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_FILE.
+if ( ! defined( 'NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_FILE' ) ) {
+	define( 'NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_FILE', __FILE__ );
+}
+
+// Define NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_DIR.
+if ( ! defined( 'NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_DIR' ) ) {
+	define( 'NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_DIR', dirname( plugin_basename( NEWSPACK_PRINT_CIRC_INTEGRATION_PLUGIN_FILE ) ) );
+}
+
+require_once 'vendor/autoload.php';
